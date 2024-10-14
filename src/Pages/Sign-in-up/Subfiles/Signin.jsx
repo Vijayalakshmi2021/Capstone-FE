6a5c0d68ccb248eb60c33cf30fa191ea15c431d5
Vijayalakshmi Subramanian <84956346+Vijayalakshmi2021@users.noreply.github.com>
2024-10-14 14:02:52 +0530

    Updated

--- src/Pages/Sign-in-up/Subfiles/Signin.jsx
+++ src/Pages/Sign-in-up/Subfiles/Signin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
-import "../signInUp.css";
+import "../SignInUp.css";
 
 function Signin({ setSignin, setLoginState }) {
     const [email, setEmail] = useState("");
