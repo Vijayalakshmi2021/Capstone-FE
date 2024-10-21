aa3c0cc136b96cb49d0e779b66bdde1bd0f73171
Vijayalakshmi Subramanian <84956346+Vijayalakshmi2021@users.noreply.github.com>
2024-10-21 11:16:32 +0530

    udapted api

--- src/Pages/ServicesPage/Forms/Cleaning.jsx
+++ src/Pages/ServicesPage/Forms/Cleaning.jsx
@@ -43,7 +43,7 @@ function Cleaning() {
         e.preventDefault();
         try {
             const response = await axios.post(
-                "http://localhost:5000/api/cleaning",
+                "https://capstone-be-g5sw.onrender.com/api/cleaning",
                 formData
             );
 
