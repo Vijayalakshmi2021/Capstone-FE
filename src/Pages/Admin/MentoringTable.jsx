f07a88b2e5263be497d26d84d0848a665f7d652a
Vijayalakshmi Subramanian <84956346+Vijayalakshmi2021@users.noreply.github.com>
2024-10-21 11:10:07 +0530

    updated api

--- src/Pages/Admin/MentoringTable.jsx
+++ src/Pages/Admin/MentoringTable.jsx
@@ -14,7 +14,7 @@ function MentoringTable() {
     useEffect(() => {
         // Fetch data from the API endpoint
         axios
-            .get("https://capstone-be-g5sw.onrender.com")
+            .get("https://capstone-be-g5sw.onrender.com/api/mentoring")
             .then((response) => {
                 setMentoringRequests(response.data);
             })
@@ -26,7 +26,7 @@ function MentoringTable() {
     const handleStatusChange = async (id, newStatus) => {
         try {
             // Update the status of the mentoring request in the backend
-            await axios.patch(`http://localhost:5000/api/mentoring/${id}/status`, {
+            await axios.patch(`https://capstone-be-g5sw.onrender.comapi/mentoring/${id}/status`, {
                 status: newStatus,
             });
 
