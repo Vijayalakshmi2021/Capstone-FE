522aeb08217ec961a5a6bf133e22cd1b971bc029
Vijayalakshmi Subramanian <84956346+Vijayalakshmi2021@users.noreply.github.com>
2024-10-21 11:01:40 +0530

    updated api

--- src/Pages/Admin/MentoringTable.jsx
+++ src/Pages/Admin/MentoringTable.jsx
@@ -14,7 +14,7 @@ function MentoringTable() {
     useEffect(() => {
         // Fetch data from the API endpoint
         axios
-            .get("http://localhost:5000/api/mentoring")
+            .get("https://capstone-be-g5sw.onrender.com")
             .then((response) => {
                 setMentoringRequests(response.data);
             })
